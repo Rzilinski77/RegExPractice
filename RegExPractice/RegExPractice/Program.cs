027b42390d543260d97395d50a954e31c45a7f3b
Randy Zilinski <rzilinski77@gmail.com>
2020-05-22 15:31:10 -0400

    Initial Commit

---
+++ RegExPractice/RegExPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegExPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
